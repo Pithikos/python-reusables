53b011747ec5598fb01b9b51bf9ee5d8eec06bf9
pithikos <manossef@hotmail.com>
2015-03-10 14:29:25 +0000

    Example and small fix

--- web/xpath.py
+++ web/xpath.py
@@ -1,38 +1,62 @@
 '''
 Gives back an xpath that points to given element and only
 
-Takes:
-  element  -  an lxml element object
+Parameters:   
+    element  -  an lxml element object
 
-Returns:
-  xpath    -  an xpath string that points to the given element and only
+Output:
+    xpath    -  an xpath string that points to the given element and only
+
 '''
 def get_element_xpath(element):
 
     xpath = ''
     
     def element_tag(el):
-        splut = el.tag.split('}')
-        if len(splut)==2:
-            return splut[1]
+        tag = el.tag
+        if '{' and '}' in tag:
+            splut = tag.split('}')
+            if len(splut)==2:
+                return splut[1]
+            else:
+                print("ERROR: Element tag is in unknown format ('%s')" % tag)
+                return None
         else:
-            print("ERROR: Element tag is in unknown format ('%s')" % el.tag)
-            return None
+            return tag
 
     def same_tag_siblings(element):
         parent = element.getparent()
         if parent is not None:
-            return filter(lambda el: el.tag == element.tag, parent.getchildren())
+            return filter(lambda el: element_tag(el) == element_tag(element), parent.getchildren())
         else:
-            return []
+            return [element]
 
     while (element is not None):
-        twins  = same_tag_siblings(element)
+        twins = same_tag_siblings(element)
+        print(twins)
         index = twins.index(element) if len(twins)>0 else 0
-        if index == 0:
+        if len(twins)==1:
             xpath = '/%s' % element_tag(element) + xpath
         else:
             xpath = '/%s[%d]' % (element_tag(element), index+1) + xpath
         element = element.getparent()
 
     return '/' + xpath
+
+
+
+
+################################ EXAMPLE ###############################
+
+
+from selenium import webdriver
+from lxml import etree
+
+browser = webdriver.Chrome()
+browser.get('http://www.google.com')
+html = etree.HTML(browser.page_source)
+
+
+# Get xpath of the Google Search button
+button = html.xpath('//input[@value="Google Search"]')[0]
+print(get_element_xpath(button)) # --> //html/body/div[1]/div[3]/form/div[2]/div[3]/center/input[1]
