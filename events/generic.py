b176a30b34499b1c55db31b0ed0ebb31945149df
pithikos <manossef@hotmail.com>
2015-04-27 18:14:45 +0100

    Dict instead of list

--- events/generic.py
+++ events/generic.py
@@ -15,24 +15,26 @@ Usage:
 '''
 
 
+
 class Observer():
-	_observers = []
-	def __init__(self):
-		self._observers.append(self)
-		self._observed_events = []
-	def observe(self, event_name, callback_fn):
-		self._observed_events.append({'event_name' : event_name, 'callback_fn' : callback_fn})
+    _observers = []
+    def __init__(self):
+        self._observers.append(self)
+        self._observables = {}
+    def observe(self, event_name, callback):
+        self._observables[event_name] = callback
 
 
 class Event():
-	def __init__(self, event_name, *callback_args):
-		for observer in Observer._observers:
-			for observable in observer._observed_events:
-				if observable['event_name'] == event_name:
-					observable['callback_fn'](*callback_args)
-
-
-
+    def __init__(self, name, data, autofire = True):
+        self.name = name
+        self.data = data
+        if autofire:
+            self.fire()
+    def fire(self):
+        for observer in Observer._observers:
+            if self.name in observer._observables:
+                observer._observables[self.name](self.data)    
 
 
 
