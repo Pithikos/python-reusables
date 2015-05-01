27f832b801ce4b0388075ab19e0fa851f69d959b
pithikos <manossef@hotmail.com>
2015-05-01 12:15:04 +0100

    Selecting added

---
+++ selecting/README.md
@@ -0,0 +1,6 @@
+````
+select.py  -->  select and deselect from a list by using expressions like '1-3, 5' and '-5, -2'
+
+
+
+````
---
+++ selecting/select.py
@@ -0,0 +1,38 @@
+'''
+Let's you use an expression to select an item, group of items,
+or deselect from a collection.
+
+Example:
+select('-3', [1, 2, 3])          => [1, 2]
+select('1-3,5', [1, 2, 3, 4, 5]) => [1, 2, 3, 5]
+select('-1-3', [1, 2, 3, 4, 5])  => [4, 5]
+
+'''
+def select(expr, collection):
+	selectors = expr.split(',')
+	selectors = map(lambda s: s.strip(), selectors)
+
+	def get_range(selector):
+		if '-' in selector:
+			a, b = map(int, selector.split('-'))
+			return range(a, b+1)
+		else:
+			return selector
+	
+	# reductive selection
+	if filter(lambda sel: sel.startswith('-'), selectors):
+		for selector in selectors:
+			if selector.startswith('-'):
+				selector = get_range(selector[1:])
+				for n in selector:
+					collection.remove(n)
+		return collection
+		
+	# selective
+	else:
+		selected = []
+		for selector in selectors:
+			selector = get_range(selector)
+			for n in selector:
+				selected.append(n)
+		return selected
