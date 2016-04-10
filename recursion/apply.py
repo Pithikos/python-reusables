a07d91908202179c243d3385ca05c96d27ae1bcb
Pithikos <manossef@hotmail.com>
2016-04-10 22:14:48 +0100

    All apply functions work now with both dict and list

--- recursion/apply.py
+++ recursion/apply.py
@@ -3,43 +3,50 @@
 
 
 # Simple
-def apply_recursively(fn, iterable):
-    if not iterable:
-        return iterable
-    if not hasattr(iterable, '__iter__'):
-        return fn(iterable)
-    if len(iterable)>1:
-        return [apply_recursively(fn, iterable[0])] + apply_recursively(fn, iterable[1:])
-    else:
-        return [apply_recursively(fn, iterable[0])]
+def apply_recursively(fn, obj):
+    if not obj:
+        return obj
+    if type(obj) is list:
+        if len(obj)>1:
+            return [apply_recursively(fn, obj[0])] + apply_recursively(fn, obj[1:])
+        else:
+            return [apply_recursively(fn, obj[0])]
+    if type(obj) is dict:
+        for k in obj:
+            obj[k] = apply_recursively(fn, obj[k])
+    return fn(obj)
 
 
 # With predicate
-def apply_recursively(fn, iterable, predicate=lambda x: True):
-    if not iterable:
-        return iterable
-    if not hasattr(iterable, '__iter__'):
-        if predicate(iterable):
-           return fn(iterable)
+def apply_recursively(fn, obj, predicate=lambda x: True):
+    if not obj:
+        return obj
+    if type(obj) is list:
+        if len(obj)>1:
+            return [apply_recursively(fn, obj[0], predicate)] +\
+                    apply_recursively(fn, obj[1:], predicate)
         else:
-            return iterable
-    if len(iterable)>1:
-        return [apply_recursively(fn, iterable[0], predicate)] +\
-                apply_recursively(fn, iterable[1:], predicate)
+            return [apply_recursively(fn, obj[0], predicate)]
+    if type(obj) is dict:
+        for k in obj:
+            obj[k] = apply_recursively(fn, obj[k], predicate)
+            #return obj[k]
+    if type(obj) is not dict and predicate(obj):
+        return fn(obj)
     else:
-        return [apply_recursively(fn, iterable[0], predicate)]
+        return obj
 
 
 # Apply to leaves of a mixed structure (dict + list)
 def apply_leaves(struct, fn=lambda x: x, predicate=lambda x: True):
     if type(struct) is list:
         if len(struct) > 1:
-            return [traverse_struct(struct[0], fn, predicate)] + traverse_struct(struct[1:], fn, predicate)
+            return [apply_leaves(struct[0], fn, predicate)] + apply_leaves(struct[1:], fn, predicate)
         else:
-            return [traverse_struct(struct[0], fn, predicate)]
+            return [apply_leaves(struct[0], fn, predicate)]
     elif type(struct) is dict:
         for k,v in struct.iteritems():
-            struct[k] = traverse_struct(v, fn, predicate)
+            struct[k] = apply_leaves(v, fn, predicate)
     else:
         if predicate(struct):
             return fn(struct)
@@ -48,10 +55,11 @@ def apply_leaves(struct, fn=lambda x: x, predicate=lambda x: True):
     return struct
 
 
+
 ############################## Examples ################################
 
-apply_recursively(str, [[1, 2], 3])                      # => [['1', '2'], '3']
-apply_recursively(int, [['1', 'a'], '2'], str.isdigit)   # => [[1, 'a'], 2]
+print(apply_recursively(str, [[1, {'a' : 2}], 3]))  # => [['1', "{'a': '2'}"], '3']
+print(apply_recursively(int, [['1', {'a' : '2'}], '3'], str.isdigit)) # => [[1, {'a': 2}], 3]
 
-traverse_struct([{'a' : 1, 'b' : 4}, [2, 3]], lambda x: 'TEST', lambda x: x>2)
+print(apply_leaves([{'a' : 1, 'b' : 4}, [2, 3]], lambda x: 'TEST', lambda x: x>2))
 # => [{'a': 1, 'b': 'TEST'}, [2, 'TEST']]
