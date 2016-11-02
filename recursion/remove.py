e7a4926f183aebceae64e74386a713c4d87a7eb1
Pithikos <manossef@hotmail.com>
2016-11-02 13:22:35 +0000

    Make it possible to recursively delete keys

---
+++ recursion/remove.py
@@ -0,0 +1,47 @@
+def remove_keys(obj, key=None, value=None):
+    """
+    Remove all dict keys recursively in an arbitrary structure.
+
+    Takes:
+        obj(iterable):   structure to be parsed
+        value(function): predicate that needs matching against a value
+        key(function):   predicate that needs matching against a key
+
+    Return:
+        Updated object
+    """
+    if type(obj) is list:
+        if len(obj)>1:
+            return [remove_keys(obj[0], key, value)] + remove_keys(obj[1:], key, value)
+        else:
+            return [remove_keys(obj[0], key, value)]
+    elif type(obj) is dict:
+        for k,v in obj.items():
+            if type(v) is dict or type(v) is list:
+                obj[k] = remove_keys(v, key, value)
+            if key and key(k) or value and value(v):
+                del obj[k]
+        return obj
+    else:
+        return obj
+
+
+
+
+
+# --------------------------------- Tests --------------------------------------
+from copy import deepcopy as clone
+
+nested = {
+    "h1" : {
+        "h2"  : 'test1',
+        "h22" : 'test2',
+        "h3"  : {
+            "h4" : 'test3'
+        }
+    }
+}
+assert remove_keys([1]) == [1]
+assert remove_keys(clone(nested), key=lambda k: k=='h22') == {'h1': {'h2': 'test1', 'h3': {'h4': 'test3'}}}
+assert remove_keys(clone(nested), key=lambda k: k=='h3') == {'h1': {'h2': 'test1', 'h22': 'test2'}}
+assert remove_keys(clone(nested), value=lambda v: v=='test2') == {'h1': {'h2': 'test1', 'h3': {'h4': 'test3'}}}
