f588839ad078ec4dbedce2fb9c18d052f1ed5417
Pithikos <manossef@hotmail.com>
2016-11-02 08:44:42 +0000

    Refactoring of apply to different functions

--- recursion/apply.py
+++ recursion/apply.py
@@ -1,48 +1,97 @@
-def apply(obj, val_apply=None, val_predicate=None, key_apply=None, key_predicate=None):
+def _apply_recursively_(obj, val_apply=None, val_predicate=None,
+                             key_apply=None, key_predicate=None,
+                             kv_apply=None):
     """
     Apply recursively to keys and/or values that meet corresponding predicates.
 
-    This will be applied to any elements of dictionaries and lists recursively.
-    Notice that this has not been optimized in any way and should thus be used
-    responsibly.
+    This will be applied to any elements of dictionaries and lists recursively
+    in-place. The traversal is done depth first in order to search the whole
+    structure deeply. No optimizations are being made so this should be used
+    carefully.
 
     Takes:
-        obj(iterable):           the structure that will be parsed
-        val_apply(function):     function that will be applied on values
-        val_predicate(function): predicate that needs matching against a value
-                                 for a value or key to be applied
-        key_apply(function):     function that will be applied on keys
-        key_predicate(function): predicate that needs matching against a key
-                                 in order for a value or key to be applied
+        obj(iterable):            The structure that will be parsed
+        val_apply(function):      Function that will be applied on values
+        val_predicate(function):  Predicate that needs matching against a value
+                                  for a value or key to be applied
+        key_apply(function):      Function that will be applied on keys
+        key_predicate(function):  Predicate that needs matching against a key
+                                  in order for a value or key to be applied
+        kv_apply(function):       Function applied to key-value pairs in a dict
 
     Return:
         Updated object
     """
+    apply = _apply_recursively_
     val_predicate = val_predicate or (lambda v: True)
     val_apply     = val_apply     or (lambda v: v)
     key_apply     = key_apply     or (lambda k: k)
     if type(obj) is list:
         if len(obj)>1:
-            return [apply(obj[0], val_apply, val_predicate, key_apply, key_predicate)] +\
-                    apply(obj[1:], val_apply, val_predicate, key_apply, key_predicate)
+            return [apply(obj[0], val_apply, val_predicate, key_apply, key_predicate, kv_apply)] +\
+                    apply(obj[1:], val_apply, val_predicate, key_apply, key_predicate, kv_apply)
         else:
-            return [apply(obj[0], val_apply, val_predicate, key_apply, key_predicate)]
+            return [apply(obj[0], val_apply, val_predicate, key_apply, key_predicate, kv_apply)]
     elif type(obj) is dict:
         for k,v in obj.items():
+            if type(v) is dict or type(v) is list:
+                obj[k] = apply(v, val_apply, val_predicate, key_apply, key_predicate, kv_apply)
             if not key_predicate or key_predicate(k):
                 del obj[k]
                 k = key_apply(k)
-                if type(v) is dict or type(v) is list:
-                    obj[k] = apply(v, val_apply, val_predicate, key_apply, key_predicate)
-                elif val_predicate(v):
+                if val_predicate(v):
                     obj[k] = val_apply(v)
                 else:
                     obj[k] = v
+                if kv_apply:
+                    obj[k] = kv_apply(k, v)
         return obj
     elif not key_predicate and val_predicate(obj):
         return val_apply(obj)
     else:
         return obj
+
+
+def apply_on_keys(iterable, apply, key=None, value=None):
+    """Apply recursively on keys of an arbitrary structure
+
+    Takes:
+        iterable: structure to apply in-place
+        apply: function to run on matching keys
+        value: function to select based on value
+        key: function to select based on key
+    """
+    return _apply_recursively_(iterable, key_apply=apply, key_predicate=key,
+                                                          val_predicate=value)
+
+
+def apply_on_values(iterable, apply, key=None, value=None):
+    """Apply recursively on values of an arbitrary structure
+
+    Takes:
+        iterable: structure to apply in-place
+        apply: function to run on matching values
+        value: function to select based on value
+        key: function to select based on key
+    """
+    return _apply_recursively_(iterable, val_apply=apply, key_predicate=key,
+                                                          val_predicate=value)
+
+
+def apply_on_dicts(iterable, apply, key=None, value=None):
+    """Apply recursively on dicts of an arbitrary structure
+
+    This is similar to other apply functions with the main difference
+    that a key-value pair is used against the passed apply function.
+
+    Takes:
+        iterable: structure to apply in-place
+        apply: function to run on matching values
+        value: function to select based on value
+        key: function to select based on key
+    """
+    return _apply_recursively_(iterable, kv_apply=apply, key_predicate=key,
+                                                         val_predicate=value)
 
 
 def apply_leaves(struct, fn=lambda x: x, predicate=lambda x: True):
@@ -90,31 +139,49 @@ def apply_leaves_by_key(struct, key_select=lambda k: k, apply=lambda v: v):
 
 # --------------------------------- Tests --------------------------------------
 
-assert apply([1], str) == ['1']
-assert apply([[1, {'a' : 2}], 3], str) == [['1', {'a': '2'}], '3']
-assert apply([['1', {'a' : '2'}], '3'], int, str.isdigit) == [[1, {'a': 2}], 3]
-assert apply([['1', {'a' : '2'}], '3'], int, key_predicate=lambda k: k=='a') == [['1', {'a': 2}], '3']
+assert _apply_recursively_([1], str) == ['1']
+assert _apply_recursively_([[1, {'a' : 2}], 3], str) == [['1', {'a': '2'}], '3']
+assert _apply_recursively_([['1', {'a' : '2'}], '3'], int, str.isdigit) == [[1, {'a': 2}], 3]
+assert _apply_recursively_([['1', {'a' : '2'}], '3'], int, key_predicate=lambda k: k=='a') == [['1', {'a': 2}], '3']
 
-assert apply([[1, {'a' : 2}], 3],
+assert _apply_recursively_([[1, {'a' : 2}], 3],
              key_apply=lambda k: 'test') == [[1, {'test' : 2}], 3]
-assert apply({'a' : 1, 'b' : 2},
+assert _apply_recursively_({'a' : 1, 'b' : 2},
              key_apply=lambda k: k+'test',
              key_predicate=lambda k: k=='b') == {'a': 1, 'btest': 2}
-assert apply({'a' : 1, 'b' : 2},
+assert _apply_recursively_({'a' : 1, 'b' : 2},
              key_apply=lambda k: k+'test',
              key_predicate=lambda k: k=='a') == {'atest': 1, 'b': 2}
-assert apply([{'a' : 1, 'b' : 4}, [2, 3]],
+assert _apply_recursively_([{'a' : 1, 'b' : 4}, [2, 3]],
              val_apply=lambda v: v*2,
              key_predicate=lambda k: k=='b') == [{'a' : 1, 'b' : 8}, [2, 3]]
-assert apply([{'a' : 1, 'b' : 4}, [2, 3]],
+assert _apply_recursively_([{'a' : 1, 'b' : 4}, [2, 3]],
              val_apply=lambda v: v*2,
              val_predicate=lambda v: v<5,
              key_predicate=lambda k: k=='b') == [{'a' : 1, 'b' : 8}, [2, 3]]
-assert apply([{'a' : 1, 'b' : 4}, [2, 3]],
+assert _apply_recursively_([{'a' : 1, 'b' : 4}, [2, 3]],
              val_apply=lambda v: v*2,
              val_predicate=lambda v: v>5,
              key_predicate=lambda k: k=='b') == [{'a' : 1, 'b' : 4}, [2, 3]]
-assert apply([{'a' : 1, 'b' : 4}, [2, 3]],
+assert _apply_recursively_([{'a' : 1, 'b' : 4}, [2, 3]],
              val_apply=lambda v: v*2,
              val_predicate=lambda v: v>5,
              key_predicate=lambda k: k=='a') == [{'a' : 1, 'b' : 4}, [2, 3]]
+
+assert apply_on_values({'a' : 1, 'b' : 4}, str, key=lambda k:k=='b') == {'a' : 1, 'b' : '4'}
+assert apply_on_keys({'a' : 1, 'b' : 4}, lambda k: k+k, key=lambda k:k=='b') == {'a' : 1, 'bb' : 4}
+
+
+nested = {
+    "h1" : {
+        "h2"  : 'test1',
+        "h22" : 'test2',
+        "h3"  : {
+            "h4" : 'test3'
+        }
+    }
+}
+assert apply_on_dicts(nested, apply=lambda k,v: k+v['h4'], key=lambda k: k=='h3')\
+                   == {'h1': {'h2': 'test1', 'h3': 'h3test3', 'h22': 'test2'}}
+assert apply_on_values(nested, apply=lambda v: 'DONE', key=lambda k: k=='h3')\
+                   == {'h1': {'h2': 'test1', 'h3': 'DONE', 'h22': 'test2'}}
