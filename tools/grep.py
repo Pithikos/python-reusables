52e06fcf5db43d6678bcfa6dbe8b0b4f32b968d6
Manos S.H <manossef@gmail.com>
2017-11-23 16:05:49 +0000

    Allow reverse grepping

--- tools/grep.py
+++ tools/grep.py
@@ -1,12 +1,16 @@
 import re
 
 
-def grep(pattern, text):
+def grep(pattern, text, reverse=False):
 	""" Grep in text with regex or simple string """
 	lines = text.splitlines()
 	new_lines = []
 	for line in lines:
-		if re.match(pattern, line) or pattern in line:
+		if reverse:
+			conditional = lambda x: not x
+		else:
+			conditional = lambda x: x
+		if conditional(re.match(pattern, line) or pattern in line):
 			new_lines.append(line)
 	return '\n'.join(new_lines)
 
@@ -21,3 +25,7 @@ assert grep('is', 'this is cool') == 'this is cool'
 assert not grep(r'^is', 'this is cool') == 'this is cool'
 assert grep(r'^this', 'this is cool') == 'this is cool'
 assert not grep(r'$this', 'this is cool')
+
+assert grep(r'^is', 'this is cool', reverse=True) == 'this is cool'
+assert not grep(r'^this', 'this is cool', reverse=True) == 'this is cool'
+assert grep(r'$this', 'this is cool', reverse=True)
